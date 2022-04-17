2529d722b6b66f498e524a232ac6fbb3918fed4d
Caleb Cartwright <caleb.cartwright@outlook.com>
2022-04-17 10:05:23 -0500

    tests: finish wiring up boilerplate

---
+++ tests/unit/test_version.py
@@ -0,0 +1,7 @@
+# Bit of a dummy stub in order to satiate pycov until we add
+# real code. Feel free to delete me, future humans
+from anrufungen import version
+
+
+def test_foo():
+    assert version.__version__ is not None
